5cd3fb06a9dc1e8d6f0afddcf23cc92654e5f49a
gabor <csgabor98@gmail.com>
2019-04-01 21:39:35 +0200

    migrációs fájlok hozzáadása

---
+++ database/migrations/2019_04_01_185214_create_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('group_id');
+            $table->string('list_name', 100);
+            $table->integer('list_priority');
+            $table->timestamps();
+
+            $table->foreign('group_id')->references('group_id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lists');
+    }
+}
---
+++ database/migrations/2019_04_01_185227_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('list_id');
+            $table->string('item_name', 200);
+            $table->integer('item_count');
+            $table->integer('item_priority');
+            $table->timestamps();
+
+            $table->foreign('list_id')->references('id')->on('lists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
